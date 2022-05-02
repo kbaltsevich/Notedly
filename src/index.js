4e74d7ac9032e1104b8472c5ab0c641b594c7040
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2022-05-02 20:26:56 +0300

    feat: add Apollo Server, and GraphQL Playground

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//схемы GraphQL
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+//функции разрешения для полей схемы
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world'
+  }
+};
+
+//Настройка Apollo Server
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.applyMiddleware({ app, path: '/api' });
+
 app.get('/', (req, res) => res.send('Hello World'));
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+  console.log(
+    `GraphQL Server running at http://localhost:${PORT}${server.graphqlPath}`
+  );
+});
