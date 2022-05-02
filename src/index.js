0ce4eef9ca105c46a49166e8b16672e46d9d46a3
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2022-05-02 20:46:50 +0300

    feat: add querys and mutations for GraphQL

--- src/index.js
+++ src/index.js
@@ -3,23 +3,56 @@ const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//mock data
+let notes = [
+  { id: '1', content: 'This is note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'This is another-another note', author: 'Riley Harrison' }
+];
+
 //схемы GraphQL
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 //функции разрешения для полей схемы
 const resolvers = {
   Query: {
-    hello: () => 'Hello world'
+    hello: () => 'Hello world',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'K.B.'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
   }
 };
 
 //Настройка Apollo Server
 const server = new ApolloServer({ typeDefs, resolvers });
-
 server.applyMiddleware({ app, path: '/api' });
 
 app.get('/', (req, res) => res.send('Hello World'));
